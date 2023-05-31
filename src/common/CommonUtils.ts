efdb81850067d41f29d169a816bf3b8d0aebf213
andubuisi225 <andubuisi225@gmail.com>
2023-05-30 23:01:20 -0700

    modified:   src/common/CommonUtils.ts

--- src/common/CommonUtils.ts
+++ src/common/CommonUtils.ts
@@ -1,8 +1,8 @@
 import { Nil } from './CommonTypes';
 
-export const APP_TITLE = 'Next-Shopper';
+export const APP_TITLE = 'my ecommerce project';
 export const APP_DESCRIPTION = `${APP_TITLE} is a simple fullstack e-commerce website demo built with Next.js`;
-export const APP_REPOSITORY_URL = 'https://github.com/onderonur/next-shopper';
+export const APP_REPOSITORY_URL = 'https://github.com/flame225/nextjs-shopper-main';
 
 export const createMockArray = (length: number) => {
   return Array.from(Array(length).keys());
